1d39534a9e384362787c7d34eb50be4fd1725c77
Marco Antonio da Silva Ferreira <masfgroove2012@gmail.com>
2025-05-13 14:08:01 -0300

    Primeiro commit do projeto Vue.js 3

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<!-- SCRIPT -->
+<script setup>
+  //Variaveis
+  let nome = 'Alice';
+  let idade = 25;
+  let numero1 = 10;
+  let numero2 = 2;
+</script>
+
+<!-- HTML -->
+<template>
+  <p>{{ ' Ola '  + nome + ' você tem ' + idade + 'anos.' }}</p>
+  <p>{{  numero1+numero2 }}</p>
+</template>
